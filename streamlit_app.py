1608e532c37d860a574208f0d476683307ca9d19
Keri Williams <kwilliams4545@gmail.com>
2022-09-26 11:44:05 -0500

    Update streamlit_app.py
    added advice header

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 sl.dataframe(fruits_to_show)
 
 # Displaying fruityvice api response
+sl.header('Fruityvice Fruit Advice!')
+
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
-sl.text(fruityvice_response)
+sl.text(fruityvice_response.json())
