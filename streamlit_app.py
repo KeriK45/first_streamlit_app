186c862a80f79e4066d928192f1e39ceae68f3ba
Keri Williams <kwilliams4545@gmail.com>
2022-09-26 11:39:12 -0500

    Update streamlit_app.py
    added requests text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as sl
 import pandas as pd
+import requests as rq
+
+fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
+sl.text(fruityvice_response)
 
 my_fruit_list_df = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list_df.sort_values(by=["Fruit"])
